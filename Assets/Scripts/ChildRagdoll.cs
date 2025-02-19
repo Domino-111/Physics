456c5cb8d763c2c2ee6efe3835bcb66eb54fdcd7
Domino-111 <dom.walsh@outlook.com>
2025-02-19 16:24:46 +1100

    Week3

--- Assets/Scripts/ChildRagdoll.cs
+++ Assets/Scripts/ChildRagdoll.cs
@@ -4,7 +4,13 @@ public class ChildRagdoll : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        GetComponentInParent<RagdollMe>()?.Ragdoll();
-        enabled = false;
+        RagdollMe other = collision.gameObject.GetComponentInParent<RagdollMe>();
+        RagdollMe self = GetComponentInParent<RagdollMe>();
+
+        if (self != null && self != other)
+        {
+            self.Ragdoll(collision.relativeVelocity);
+            enabled = false;
+        }
     }
 }
--- Assets/Scripts/RagdollMe.cs
+++ Assets/Scripts/RagdollMe.cs
@@ -15,22 +15,28 @@ public struct JointData
 public class RagdollMe : MonoBehaviour
 {
     Animator animator;
-    Joint[] jointData;
+    public JointData[] jointData;
 
     void Start()
     {
-        GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+
+        GetAllJoints();
+        AddChildRagdoll();
+        SetChildColliders(false);
     }
 
     private void Update()
     {
         float scoreTotal = 0f;
 
-        for (int i = 0; i < jointData.Length; i ++)
+        for (int i = 0; i < jointData.Length; i++)
         {
             var joint = jointData[i];
 
             float newScore = Mathf.Abs(joint.previousForce - joint.joint.currentForce.magnitude);
+
+            scoreTotal += newScore;
 
             joint.previousForce = joint.joint.currentForce.magnitude;
         }
@@ -40,19 +46,57 @@ public class RagdollMe : MonoBehaviour
     {
         Joint[] joints = GetComponentsInChildren<Joint>();
 
-        JointData = new JointData[joints.Length];
+        jointData = new JointData[joints.Length];
 
-        for (int i = 0; i < jointData.Length; i ++)
+        for (int i = 0; i < jointData.Length; i++)
         {
             Joint joint = joints[i];
             jointData[i] = new JointData(joint);
         }
 
-        return;
+        return joints;
+    }
+
+    void AddChildRagdoll()
+    {
+        foreach (var rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.gameObject.AddComponent<Rigidbody>();
+        }
+    }
+
+    void SetChildColliders(bool enabled)
+    {
+        Collider currentCol = GetComponent<Collider>();
+
+        foreach (var col in GetComponentsInChildren<Collider>())
+        {
+            if (col == currentCol)
+            {
+                currentCol.enabled = !enabled;
+            }
+
+            else 
+            {
+                col.enabled = enabled;
+            }
+        }
+    }
+
+    public void Ragdoll(Vector3 impact)
+    {
+        if (impact.magnitude < 3f)
+        {
+            return;
+        }
+
+        SetChildColliders(true);
+
+        animator.enabled = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        animator.enabled = false;
+        Ragdoll(collision.relativeVelocity);
     }
 }
