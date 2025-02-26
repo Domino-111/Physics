43bc2ea571e4a1484ee56dac44774c3f4894bac2
Domino-111 <dom.walsh@outlook.com>
2025-02-26 15:49:25 +1100

    Week4

---
+++ Assets/Scripts/FrameRate.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FrameRate : MonoBehaviour
+{
+    void Start()
+    {
+        Application.targetFrameRate = 24;
+    }
+
+    void Update()
+    {
+        
+    }
+}
